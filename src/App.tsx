9fcecb9892c12ff256c1ce689a649970160324cb
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-03-30 16:21:26 -0400

    React state&Typescript(interface)

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+interface IState {
+  counter: number;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter: 0
+  };
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        {counter}
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+
+  add = () => {
+    // this.setState({ hello: 1 }); IState doesn't have 'hello'
+    this.setState(prev => {
+      return {
+        counter: prev.counter + 1
+      };
+    });
+  };
+}
+
+export default App;
